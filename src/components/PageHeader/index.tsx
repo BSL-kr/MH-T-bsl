388b7f68c116cd9f25c6d99eaa022da9b32c7535
bsl <bigcoms6290@gmail.com>
2024-09-28 18:18:14 +0900

    fix: page header

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -2,15 +2,19 @@ import styled from 'styled-components';
 
 const PageHeaderEle = styled.header<{ isUseDescription: boolean }>`
   margin-bottom: ${props => (props.isUseDescription ? '13px' : '36px')};
+
   & > h2 {
     font-size: 20px;
     line-height: 25px;
     text-align: left;
+    margin-bottom: 13px;
   }
 
   & > p {
     font-size: 16px;
     line-height: 18px;
+    color: #595959;
+    text-align: left;
   }
 `;
 
