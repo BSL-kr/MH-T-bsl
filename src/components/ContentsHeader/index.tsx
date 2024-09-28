019b6acd6605d137aa0f4fb461bcb52f3e0fdee6
bsl <bigcoms6290@gmail.com>
2024-09-28 14:33:20 +0900

    feat: Contents header

---
+++ src/components/ContentsHeader/index.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+const ContentsHeaderEle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  border-botton: 1px solid #cfcfcf;
+  padding-bottom: 15px;
+
+  .title-section {
+    display: flex;
+    align-items: center;
+  }
+
+  & > span {
+    display: inline-block;
+    width: 4px;
+    height: 17px;
+    border-radius: 5px;
+  }
+
+  & > h3 {
+    font-size: 16px;
+    line-height: 20px;
+  }
+`;
+
+const ContentsHeader = (props: { title: string; HeaderRight: ReactNode }) => {
+  const { title, HeaderRight } = props;
+
+  return (
+    <ContentsHeaderEle>
+      <div className='title-section'>
+        <span></span>
+        <h3 className='gothic-a1-bold'>{title}</h3>
+      </div>
+      {HeaderRight}
+    </ContentsHeaderEle>
+  );
+};
+
+export default ContentsHeader;
